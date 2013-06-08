e3aa7708333d8e9031e5a59bb248c382625b77a2
fablab <fablab@robair-ICONIA-W700P.(none)>
2013-06-08 09:22:36 +0200

    derniere version de la tabmette

--- scripts/encoder_publish.py
+++ scripts/encoder_publish.py
@@ -32,7 +32,7 @@ def talker():
         print "roue gauche :" + str(wheel1)
         print "roue droite :" + str(wheel2)
         pub.publish(wheel2,wheel1)
-        rospy.sleep(0.2)
+        rospy.sleep(0.02)
 
 
 if __name__ == '__main__':
