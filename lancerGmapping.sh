66a0e60f2d8d09201f33c9aef8a5911795f9d2da
fablab <fablab@robair-ICONIA-W700P.(none)>
2013-06-11 14:20:08 +0200

    gmapping

--- lancerGmapping.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-gnome-terminal -x bash -c "roscore; sleep 10000"
-sleep 2
-gnome-terminal -x bash -c "rosrun robair_demo arduino_sensors.py; sleep 10000"
-gnome-terminal -x bash -c "rosrun hokuyo_node hokuyo_node; sleep 10000"
-gnome-terminal -x bash -c "rosrun robair_demo odometry; sleep 10000"
-sleep 2
-roslaunch robair_demo slamGmappingLidar.launch
--- src/odometry.cpp
+++ src/odometry.cpp
@@ -105,7 +105,7 @@ int main(int argc, char **argv)
    // subscribing to /encoder
    ros::Subscriber sub = n.subscribe("encoder", 1000, calculOdometry);
 
-   ros::Rate loop_rate(50);
+   ros::Rate loop_rate(100);
 
    ROS_INFO("TICK_TO_RAD : %lf", TICK_TO_RAD);
    ROS_INFO("TICKS_TO_M : %lf", TICKS_TO_M);
