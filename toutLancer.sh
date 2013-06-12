ab91fed2cd5d5e422d8fcfd9089f00628ac49a45
fablab <fablab@robair.(none)>
2013-06-12 15:35:46 +0200

    ajout de la visualisation de la photo sur la tablette

--- toutLancer.sh
+++ toutLancer.sh
@@ -9,3 +9,4 @@ sleep 3
 export GSCAM_CONFIG="v4l2src device=/dev/video0 ! video/x-raw-rgb,width=320,height=240,framerate=(fraction)30/1 ! ffmpegcolorspace"
  gnome-terminal -x bash -c "rosrun gscam gscam"
  gnome-terminal -x bash -c "rosrun mjpeg_server mjpeg_server"
+ gnome-terminal -x bash -c "cd /home/fablab/ros_workspace/robair_demo/scripts; ./robair_print_pic.py static/images/photo.jpg"
