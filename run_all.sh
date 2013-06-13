c0c99beed14e93f616c8d75fdace651053c338bc
fablab <fablab@robair.(none)>
2013-06-13 10:32:21 +0200

    modifs

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,5 @@
  #!/bin/bash
-export GSCAM_CONFIG="v4l2src device=/dev/video0 ! video/x-raw-rgb,width=320,height=240,framerate=(fraction)30/1 ! ffmpegcolorspace"
+export GSCAM_CONFIG="v4l2src device=/dev/video0 ! video/x-raw-rgb,width=640,height=480,framerate=(fraction)10/1 ! ffmpegcolorspace"
  gnome-terminal -x bash -c "roslaunch robair_demo run_all.launch"
  gnome-terminal -x bash -c "cd /home/fablab/ros_workspace/robair_demo/scripts/; ./robair_print_pic.py static/images/photo.jpg"
  
