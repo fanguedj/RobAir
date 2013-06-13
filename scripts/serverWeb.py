bcafab9bd992fc9b5738d8c979e94bf63c732d96
fablab <fablab@robair.(none)>
2013-06-13 18:26:30 +0200

    presque

--- scripts/serverWeb.py
+++ scripts/serverWeb.py
@@ -12,6 +12,7 @@ from gevent.pywsgi import WSGIServer
 from flask import Flask, request, render_template,session, redirect
 
 from robair_demo.msg import Command
+from geometry_msgs.msg import Pose
 
 
 class TabletNode(threading.Thread):
@@ -19,19 +20,24 @@ class TabletNode(threading.Thread):
     def __init__(self, topic='/cmd', node_name="tablet"):
         self.node_name = node_name
         self.pub = rospy.Publisher(topic, Command)
+        self.pubPos = rospy.Publisher("/goal_from_tablet", Pose)
         rospy.init_node('tablet')
 
 
     def key_pressed(self, key):
 	if (not rospy.is_shutdown()):
-	  directions = {  "top": 0,
-                        "bottom": 1,
-                        "left": 2,
-                        "right": 3,
-                        "s": 4}
-	  if key in directions.keys():
-	      print("%s" % key)
-	      self.pub.publish(Command(directions[key]))
+            directions = {"top": 0,
+                          "bottom": 1,
+                          "left": 2,
+                          "right": 3,
+                          "s": 4}
+            if key in directions.keys():
+                print("%s" % key)
+                self.pub.publish(Command(directions[key]))    
+            if "pos" in key:
+                val = key.split("_")
+                print("position d'arrivee : %s" % key)
+                self.pubPos.publish(Pose((0.0, 0.0, 0.0), (0.0, 0.0, 0.0, 0.0)))
 
 
 tablet_node = TabletNode()
@@ -41,11 +47,11 @@ app.secret_key = 'blablabla'
 @app.route('/')
 def reserver():
     return render_template('reservation.html')
-  
+
 @app.route('/commande')
 def index():
     return render_template('navigation.html')
- 
+
 @app.route('/commande/api')
 def goApi():
     api();
@@ -59,7 +65,7 @@ def api():
             tablet_node.key_pressed(message)
             ws.send(message)
     return
- 
+
 if __name__ == '__main__':
     address = "127.0.0.1", 4243
     http_server = WSGIServer(address, app, handler_class=WebSocketHandler)
--- scripts/static/js/websockets1.js
+++ scripts/static/js/websockets1.js
@@ -1,45 +1,53 @@
 $(document).ready(function() {
 
-	if ("WebSocket" in window) {
-		var loc = window.location, new_uri;
-		if (loc.protocol === "https:") {
-			new_uri = "wss:";
-		} else {
-			new_uri = "ws:";
-		}
-		new_uri += "//" + loc.host;
-		new_uri += loc.pathname + "/api";
-		ws = new WebSocket(new_uri);
-		ws.onmessage = function(msg) {
-			$("#log").append("<p>" + msg.data + "</p>")
-		};
+    if ("WebSocket" in window) {
+	var loc = window.location, new_uri;
+	if (loc.protocol === "https:") {
+	    new_uri = "wss:";
 	} else {
-		alert("WebSocket not supported");
+	    new_uri = "ws:";
 	}
+	new_uri += "//" + loc.host;
+	new_uri += loc.pathname + "/api";
+	ws = new WebSocket(new_uri);
+	ws.onmessage = function(msg) {
+	    $("#log").append("<p>" + msg.data + "</p>")
+	};
+    } else {
+	alert("WebSocket not supported");
+    }
 });
 
 //Sending methods for directions.
 function goTop() {
-	ws.send("top")
-	return true;
+    ws.send("top")
+    return true;
 }
 
 function goBack() {
-	ws.send("bottom")
-	return true;
+    ws.send("bottom")
+    return true;
 }
 
 function goRight() {
-	ws.send("right");
-	return true;
+    ws.send("right");
+    return true;
 }
 
 function goLeft() {
-	ws.send("left");
-	return true;
+    ws.send("left");
+    return true;
 }
 
 function stopMove() {
-	ws.send("s");
-	return true;
+    ws.send("s");
+    return true;
+}
+
+//Methode d'envoie d'une position de destination
+function goThere(x, y) {
+    var x_string = x.toString();
+    var y_string = y.toString();
+    ws.send("pos" + "_" + x_string + "_" + y_string);
+    return true;
 }
